6b0e53a19b7685a4e9fd2a3d071cc4bb74a72d4b
TensorLong <tensorshen@gmail.com>
2026-05-23 11:06:48 +0800

    Tune pointed OCR dwell to 130 ms for v3.3.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.yiqun.translator"
         minSdk = 23
         targetSdk = 35
-        versionCode = 30314
-        versionName = "3.3.6.1"
+        versionCode = 30315
+        versionName = "3.3.6.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     bundle {
